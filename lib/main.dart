874db0dd9a0ea7ba6bbd024479d286341e5e9925
Alex Naupay <alexnaupay@gmail.com>
2025-04-17 22:18:04 -0500

    feat: Initial commit - Add app theme and update iOS configuration
    This commit introduces the initial application theme for both light and dark modes, defining colors, typography, button styles, and other UI elements. It also includes updates to the iOS configuration files, such as the Podfile and project settings, to integrate these changes.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:personal_finance_flutter/app_theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,26 +11,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final bool isDarkMode = MediaQuery.of(context).platformBrightness == Brightness.dark;
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
+      theme: AppTheme.greenFinanceTheme,
+      darkTheme: AppTheme.greenFinanceDarkTheme,
+      themeMode: isDarkMode? ThemeMode.dark : ThemeMode.light,
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
